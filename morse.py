e2d86c9bceb11dbfd67f7800c8807f7814ed332e
Hippasus <zhuyanggu@gmail.com>
2013-02-23 12:10:09 +0800

    code refactored

--- morse.py
+++ morse.py
@@ -13,11 +13,13 @@ char_code_map = {
 	"6": "-....",	"7": "--...",	"8": "---..",	"9": "----.",	"0": "-----",
 
 	".": ".-.-.-",	",": "--..--",	"?": "..--..",	"'": ".----.",
-	"/": "-..-.",	"(": "-.--.", 	")": "-.--.-",	"&": ".-...",
+	"/": "-..-.",	"(": "-.--.",	")": "-.--.-",	"&": ".-...",
 	":": "---...",	";": "-.-.-.",	"=": "-...-",	"+": ".-.-.",
 	"-": "-....-",	"_": "..--.-",	"\"": ".-..-.",	"$": "...-..-",
 	"!": "-.-.--",	"@": ".--.-."
 }
+
+word_separator_code = " / "
 
 class MorseCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -36,42 +38,57 @@ class MorseCommand(sublime_plugin.TextCommand):
 		self.view.end_edit(edit)
 
 	def convert_to_morse_code(self, edit, currentSelection):
-		original = self.view.substr(currentSelection)
-		original_lower = string.lower(original)
-		output, prev_char = "", " "
+		line_ending = self._get_line_ending()
 		invalid_char_found = False
 
-		def get_code_of_non_whitespace(char, prev_char):
-			def get_code(char):
-				try:
-					code = char_code_map[char]
-					return code
-				except KeyError:
-					invalid_char_found = True
+		def get_coded_selection():
+			original = self.view.substr(currentSelection)
+			original_lower = string.lower(original)
 
-				return None
-
-			code = get_code(char)
-			if code is None:
-				return ""
-
-			result = ""
-			if prev_char != " ":
-				result += " "
-			result += code
-
+			lines = original_lower.split(line_ending)
+			result = get_coded_lines(lines)
 			return result
 
-		for char in original_lower:
-			if char == " ":
-				if prev_char == char:
-					continue
+		def get_coded_lines(lines):
+			l, coded_lines = len(lines), ""
 
-				output += " / "
-			else:
-				output += get_code_of_non_whitespace(char, prev_char)
+			for line in lines:
+				coded_lines += get_coded_line(line)
+				coded_lines += line_ending
+			if l > 0:
+				coded_lines = coded_lines[:-len(line_ending)]
+			return coded_lines
 
-			prev_char = char
+		def get_coded_line(line):
+			words = line.split()
+			l, coded_line = len(words), ""
+
+			for word in words:
+				coded_line += get_coded_word(word)
+				coded_line += word_separator_code
+			if l > 0:
+				coded_line = coded_line[:-len(word_separator_code)]
+			return coded_line
+
+		def get_coded_word(word):
+			coded_word = ""
+			for char in word:
+				coded_char, ok = get_coded_char(char)
+				coded_word += coded_char
+				if ok:
+					coded_word += " "
+			coded_word = coded_word.strip()
+			return coded_word
+
+		def get_coded_char(char):
+			try:
+				code = char_code_map[char]
+				return code, True
+			except KeyError:
+				invalid_char_found = True
+			return char, False
+
+		output = get_coded_selection()
 
 		if invalid_char_found:
 			print "invalid characters found and ignored."
@@ -79,3 +96,17 @@ class MorseCommand(sublime_plugin.TextCommand):
 		self.view.replace(edit, currentSelection, output)
 
 		return sublime.Region(currentSelection.begin(), currentSelection.begin() + len(output))
+
+	def _get_line_ending(self):
+		line_ending_setting = self.view.settings().get('default_line_ending')
+		if line_ending_setting == 'windows':
+			return '\r\n'
+		elif line_ending_setting == 'mac':
+			return '\r'
+		return '\n'
+
+	def normalize_line_endings(self, string):
+		string = string.replace('\r\n', '\n').replace('\r', '\n')
+		line_ending = self._get_line_ending()
+		string = string.replace('\n', line_ending)
+		return string 
